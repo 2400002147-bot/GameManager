204110de034af8b76f8e9fbb3b27809e4fbbce43
Thanh Hoàng <2400002147@nttu.edu.vn>
2026-04-12 15:33:31 +0700

    Add files via upload

--- QLTK_Game/GameDB.sql
+++ QLTK_Game/GameDB.sql
@@ -7,7 +7,7 @@ GO
 CREATE TABLE Users (
     Id INT PRIMARY KEY IDENTITY,
     Username NVARCHAR(50) UNIQUE,
-    Password NVARCHAR(MAX), 
+    Password NVARCHAR(50), 
     DisplayName NVARCHAR(100)
 );
 -- Tạo tài khoản Admin
@@ -15,54 +15,56 @@ INSERT INTO Users (Username, Password, DisplayName)
 VALUES ('admin', 'admin', 'ADMIN');
 go
 -- 2. Bảng Liên Quân 
--- Thường là: Garena, Facebook, Apple ID
 CREATE TABLE LienQuanAccounts (
     Id INT PRIMARY KEY IDENTITY,
     Username NVARCHAR(100),
-    Password NVARCHAR(MAX), 
+    Password NVARCHAR(50), 
     LoginType NVARCHAR(50), 
     RankLienQuan NVARCHAR(50),
     SoTuong INT,
-    Skins INT
+    Skins INT,
+	Note NVARCHAR(MAX)
 );
 --Thêm dữ liệu cho bảng Liên Quân
-INSERT INTO LienQuanAccounts (Username, Password, LoginType, RankLienQuan, SoTuong, Skins)
+INSERT INTO LienQuanAccounts (Username, Password, LoginType, RankLienQuan, SoTuong, Skins, Note)
 VALUES 
-(N'lienquan1@gmail.com', N'123456', N'Garena', N'Chiến Tướng', 115, 320),
-(N'lienquan2@gmail.com', N'123456', N'Facebook', N'Cao Thủ', 105, 150);
+(N'lienquan1@gmail.com', N'123456', N'Garena', N'Chiến Tướng', 115, 320, N'Tài khoản chính'),
+(N'lienquan2@gmail.com', N'123456', N'Facebook', N'Cao Thủ', 105, 150, N'Acc phụ 1');
 go
 -- 3. Bảng Free Fire 
 CREATE TABLE FreeFireAccounts (
     Id INT PRIMARY KEY IDENTITY,
     Username NVARCHAR(100),
-    Password NVARCHAR(MAX),
+    Password NVARCHAR(50),
     LoginType NVARCHAR(50), 
     ID_InGame NVARCHAR(20),
     SkinSungHiem NVARCHAR(MAX),
-    LevelAccount INT
+    LevelAccount INT,
+	Note NVARCHAR(MAX)
 );
 
 --Thêm dữ liệu cho bảng Free Fire 
-INSERT INTO FreeFireAccounts (Username, Password, LoginType, ID_InGame, SkinSungHiem, LevelAccount)
+INSERT INTO FreeFireAccounts (Username, Password, LoginType, ID_InGame, SkinSungHiem, LevelAccount, Note)
 VALUES 
-(N'ff001@gmail.com', N'ffpass1', N'Google', N'99887766', N'500', 70),
-(N'ffnewbie@gmail.com', N'ffpass2', N'Garena', N'11223344', N'50', 15);
+(N'ff001@gmail.com', N'ffpass1', N'Google', N'99887766', N'500', 70, N'Tài khoản chính'),
+(N'ffnewbie@gmail.com', N'ffpass2', N'Garena', N'11223344', N'50', 15, N'Tài khoản phụ');
 go
 -- 4. Bảng FC Mobile 
 CREATE TABLE FCMobileAccounts (
     Id INT PRIMARY KEY IDENTITY,
     Username NVARCHAR(100),
-    Password NVARCHAR(MAX),
+    Password NVARCHAR(50),
     LoginType NVARCHAR(50), 
     DoiHinh_OVR INT,
     GiaTriDoiHinh BIGINT,
-    Region NVARCHAR(50)
+    Region NVARCHAR(50),
+	Note NVARCHAR(MAX)
 );
 --Thêm dữ liệu cho bảng FC Mobile 
-INSERT INTO FCMobileAccounts (Username, Password, LoginType, DoiHinh_OVR, GiaTriDoiHinh, Region)
+INSERT INTO FCMobileAccounts (Username, Password, LoginType, DoiHinh_OVR, GiaTriDoiHinh, Region,Note)
 VALUES 
-(N'fc01@gmail.com', N'fcpass1', N'Facebook', 140, 25000000000, N'VN'),
-(N'fc02@gmail.com', N'fcpass2', N'EA Account', 138, 18000000000, N'Global');
+(N'fc01@gmail.com', N'fcpass1', N'Facebook', 140, 25000000000, N'VN', N'Tài khoản chính'),
+(N'fc02@gmail.com', N'fcpass2', N'EA Account', 138, 18000000000, N'Global', N'Tài khoản phụ');
 GO
 
 
